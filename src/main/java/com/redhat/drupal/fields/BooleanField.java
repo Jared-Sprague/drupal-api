5e5877625bd8ba1bd8a15e48e6e44467c4ba79cb
jsprague <jsprague@redhat.com>
2014-04-14 11:53:09 -0400

    added BooleanField

---
+++ src/main/java/com/redhat/drupal/fields/BooleanField.java
@@ -0,0 +1,58 @@
+package com.redhat.drupal.fields;
+
+import com.redhat.drupal.Utils;
+
+public class BooleanField extends Field {
+	private Boolean value;
+	
+	public BooleanField(String machineName) {
+		this.machineName = machineName;
+	}
+	
+	public BooleanField(String machineName, String xml) {
+		this.machineName = machineName;
+		fromXml(xml);
+	}
+
+	@Override
+	public void fromXml(String xml) {
+		Integer intValue = Utils.safeNewInteger(parseField("//" + this.machineName + "//value", xml));
+		if (intValue == null || intValue == 0) {
+			this.value = false;
+		} else {
+			this.value = true;
+		}
+	}
+
+	@Override
+	protected String innerPostXml() {
+		if (!isSet()) {
+			return null;
+		}
+		
+		int intValue = this.value ? 1 : 0;
+		
+		return "<value>" + intValue + "</value>";
+	}
+	
+	@Override
+	public boolean isSet() {
+		return this.value != null;
+	}
+
+	@Override
+	protected String innerAllXml() {
+		// for this type there is no difference between post and all xml
+		return innerPostXml();
+	}
+
+	public Boolean getValue() {
+		return value;
+	}
+
+	public void setValue(Boolean value) {
+		this.value = value;
+	}
+
+	
+}
