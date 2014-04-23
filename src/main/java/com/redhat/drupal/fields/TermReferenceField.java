d0168f55e3023b07e4108685ae7aab493db62378
jsprague <jsprague@redhat.com>
2014-04-23 14:45:47 -0400

    added vocabularyName as an attribute of TermReference

--- src/main/java/com/redhat/drupal/fields/TermReferenceField.java
+++ src/main/java/com/redhat/drupal/fields/TermReferenceField.java
@@ -2,12 +2,27 @@ package com.redhat.drupal.fields;
 
 public class TermReferenceField extends IdListField {
 	private static final String VALUE_ELEMENT_NAME = "tid";
+	private String vocabularyName;
 
-	public TermReferenceField(String machineName, String xml) {
+	public TermReferenceField(String machineName, String vocabularyName, String xml) {
 		super(machineName, VALUE_ELEMENT_NAME, xml);
+		this.vocabularyName = vocabularyName;
 	}
 
-	public TermReferenceField(String machineName) {
+	public TermReferenceField(String machineName, String vocabularyName) {
 		super(machineName, VALUE_ELEMENT_NAME);
+		this.vocabularyName = vocabularyName;
+	}
+
+	public String getVocabularyName() {
+		return vocabularyName;
+	}
+
+	public void setVocabularyName(String vocabularyName) {
+		this.vocabularyName = vocabularyName;
+	}
+
+	public static String getValueElementName() {
+		return VALUE_ELEMENT_NAME;
 	}
 }
